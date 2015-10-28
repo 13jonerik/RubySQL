a41df8d298f00d860fd8a7d62b48e7164371314d
Jon-Erik <jonerik@pdx.edu>
2015-10-27 22:46:10 -0700

    Configured VPN and Test Code

--- embedded.rb
+++ embedded.rb
@@ -1,14 +1,32 @@
-require ‘pg’
+#!/usr/bin/env ruby
+
+require 'pg'
 begin
 	#create our db connection
 	connect = PGconn.connect(
-		:host => “dbclass.cs.pdx.edu”,
-		:dbname => “f15ddb41”,
-		:user => “f15ddb41”,
-		:password => “******”)
+		:host => "dbclass.cs.pdx.edu",
+		:dbname => "f15ddb41",
+		:user => "f15ddb41",
+		:password => "*")
 
 	#… do stuff on next slide …
+
+	results = connect.exec("SELECT sid
+			FROM sailors")
+
+	# print column names
+	puts results.fields().join(',')
+
+	# print each row of result set
+	results.each do |row|
+		puts row['sid']
+	end
 
 	connect.close
 
 rescue PG::Error => err
+	puts err
+end
+
+
+
