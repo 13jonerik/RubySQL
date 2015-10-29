8f1c5fb720cce6c5b58689d457542c9de17c61ee
Jon-Erik <jonerik@pdx.edu>
2015-10-29 15:56:12 -0700

    Finished Logic for agent_raise Table

--- agent.rb
+++ agent.rb
@@ -25,12 +25,23 @@ begin
 
 	# puts results[3]['salary]
 
+
+	connect.exec("DROP TABLE IF EXISTS agent_raise")
+
+	connect.exec("CREATE TABLE agent_raise (
+				agent_id INT,
+				raise INT)")
+
 	records = results.count - 2
 
 	for i in 1..records
 		ave = (results[i-1]['salary'].to_i + results[i+1]['salary'].to_i) / 2
 		if results[i]['salary'].to_i < ave then
-			# insert row into raise db
+			raiseNum = ave - results[i]['salary'].to_i
+			id    = results[i]['agent_id'].to_i
+			connect.exec_params('INSERT INTO agent_raise (agent_id, raise) VALUES ($1,$2)', [id, raiseNum])	
+			#connect.exec_prepa('addRecord', [id, raiseNum])
+			#connect.exec('DEALLOCATE addRecord')
 		end
 	end 
 
