2a3fa5a67e4cd0c1b3abc3568b68a80bd7ca005b
Jon-Erik <jonerik@pdx.edu>
2015-10-29 20:51:16 -0700

    Edge Case Handled

--- agent.rb
+++ agent.rb
@@ -14,8 +14,7 @@ begin
 		:user => "f15ddb41",
 		:password => pword)
 
-	#… do stuff on next slide …
-
+	#create a cursor on agents list
 	results = connect.exec("SELECT agent_id, salary 
 			FROM agent ORDER BY
 			salary DESC")
@@ -23,27 +22,32 @@ begin
 	# print column names
 	puts results.fields().join(',')
 
-	# puts results[3]['salary]
-
-
+	#for purpose of demo, drop table if exists and remake 
 	connect.exec("DROP TABLE IF EXISTS agent_raise")
 
+	#create the agent_raise table
 	connect.exec("CREATE TABLE agent_raise (
 				agent_id INT,
 				raise INT)")
 
-	records = results.count - 2
+	#save number of records in var numRecords
+	numRecords = results.count - 2
 
-	for i in 1..records
+	
+	#move cursor through the table, compare salaries, add to agent_raise if necessary
+	for i in 1..numRecords
 		ave = (results[i-1]['salary'].to_i + results[i+1]['salary'].to_i) / 2
 		if results[i]['salary'].to_i < ave then
 			raiseNum = ave - results[i]['salary'].to_i
 			id    = results[i]['agent_id'].to_i
 			connect.exec_params('INSERT INTO agent_raise (agent_id, raise) VALUES ($1,$2)', [id, raiseNum])	
-			#connect.exec_prepa('addRecord', [id, raiseNum])
-			#connect.exec('DEALLOCATE addRecord')
 		end
 	end 
+
+	#handle edge case - lowest paid agent gets raise equal to diff. of next highest (one tied)
+	agentLow = results[numRecords-1]['salary'].to_i - results[numRecords+1]['salary'].to_i
+	agentLowID = results[numRecords+1]['agent_id'].to_i
+	connect.exec_params('INSERT INTO agent_raise (agent_id, raise) VALUES ($1,$2)', [agentLowID, agentLow])	
 
 	connect.close
 
